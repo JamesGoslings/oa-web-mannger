5aaa4b1a2f4ab0421fecfde06525b56e3da1c542
JamesGosing <3248175240@qq.com>
2024-04-23 19:04:28 +0800

    chore: 基础依赖安装和环境配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    // 自动导入配置
+    AutoImport({
+      imports:[
+          // 预设
+          'vue'
+      ]
+    }),
+    vue()
+  ],
+  resolve:{
+    alias:{
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
