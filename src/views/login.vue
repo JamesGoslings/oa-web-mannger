6bb42cd8f6b68725e27810966973015caf50e67b
JamesGosing <3248175240@qq.com>
2024-04-24 12:37:27 +0800

    perf: 优化了登录页面

--- src/views/login.vue
+++ src/views/login.vue
@@ -7,6 +7,16 @@
             <div class="msgCardBox">
                 <div class="msgCard">
                     <div class="title">登录账号</div>
+
+                    <span class="iptBox">
+                        <div class="ico iconfont">&#xea92;</div>
+                        <input class="ipt" placeholder="用户名" v-model="userMsg.username"/>
+                    </span>
+                    <span class="iptBox">
+                        <div class="ico iconfont">&#xe64c;</div>
+                        <input class="ipt" placeholder="密码" type="password" v-model="userMsg.password"/>
+                    </span>
+                    <button class="btn" @click="login()">登录</button>
                 </div>
             </div>
         </div>
@@ -14,9 +24,14 @@
 </template>
 
 <script setup>
+let userMsg = ref({username: '',password: ''})
+function login(){
+    console.log(userMsg.value)
+}
 </script>
 
 <style lang="scss" scoped>
+@import url('/src/assets/font-icon/iconfont.css');
 .globalSty{
     position: fixed;
 	top: 0;
@@ -42,8 +57,7 @@
             align-items: center;
             justify-content: center;
             .img{
-                width: 280px;
-                height: 280px;
+                width: 90%;
             }
         }
         .msgCardBox{
@@ -58,10 +72,56 @@
                 width: 70%;
                 box-shadow: 0px 0px 8px #928c8c64;
                 border-radius: 5px;
+                display: flex;
+                justify-content: center;
+                flex-wrap: wrap;
                 .title{
                     width: 100%;
+                    height: 20%;
                     text-align: center;
                     color: rgb(235,229,225);
+                }
+                .iptBox{
+                    width: 80%;
+                    height: 15%;
+                    background: rgb(253,253,254);
+                    box-shadow: 0px 0px 2px #928c8c64;
+                    border-radius: 2px;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    .ico{
+                        width: 10%;
+                        font-size: 10px;
+                        color: rgb(235,229,225);
+                        text-align: center;
+                    }
+                    input::placeholder{
+                        font-size: 10px;
+                        color: rgb(235,229,225);
+                    }
+                    .ipt{
+                        border: 0px;
+                        background: rgb(253,253,254);
+                        outline: none;
+                        width: 80%;
+                        height: 90%;
+                        font-size: 10px;
+                        color: rgb(199, 192, 188);
+
+                    }
+                }
+                .btn{
+                    border-radius: 5px;
+                    border: 0px;
+                    background: rgb(246,98,86);
+                    width: 80%;
+                    height: 12%;
+                    font-size: 10px;
+                    color: #FFF;
+                    &:active{
+                        background: rgb(202, 131, 125);
+                    }
                 }
             }
         }
