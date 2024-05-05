2ca528d08e8e10babf442563e2d63c596400a945
JamesGosing <3248175240@qq.com>
2024-05-05 10:08:21 +0800

    feat: 完成了分配权限页面基本结构

---
+++ src/api/menu.js
@@ -0,0 +1,18 @@
+import request from '@/utils/request.js'
+
+const MENU_URL = '/admin/system/sysMenu/'
+
+// 拿到roleId对应有的菜单列表
+export function getMenuListByRoleId(roleId){
+    return request({
+        url: MENU_URL + 'toAssign/' + roleId,
+        method: 'GET'
+    })
+}
+// 拿到树型结构的所有菜单
+export function getAllTreeMenus(){
+    return request({
+        url: MENU_URL + 'getNodes',
+        method: 'GET'
+    })
+}
--- src/views/system/sysRole/assignAuth.vue
+++ src/views/system/sysRole/assignAuth.vue
@@ -1,20 +1,78 @@
 <!-- 分配权限页面 -->
 <template>
     <div class="assignAllSty">
-        <div class="funTitle" style="justify-content: center;">{{`正在为 “${role.roleName}” 分配权限`}}</div>
-        
+        <div class="funTitle">{{`正在为 “${role.roleName}” 分配权限`}}</div>
+        <div class="funMode">
+            <el-tree
+            ref="tree"
+            :data="menuTreeList"
+            node-key="id"
+            show-checkbox
+            default-expand-all
+            :props="myProps"
+            @check-change="getChooseMenusIdlist"
+            :default-checked-keys="oldMenuIdList"
+            />
+        </div>
     </div>
 </template>
 
 <script setup>
 import { useRoute } from 'vue-router'
+import {getMenuListByRoleId,getAllTreeMenus} from '@/api/menu'
 
+// 存选中的id列表
+let chooseIdList = ref([])
+// 配置树型复选框
+let myProps = ref({label: 'name'})
+// 通过监听选中与否来不断获取选中了的菜单
+function getChooseMenusIdlist(data, checked, indeterminate) {
+    const index = chooseIdList.value.indexOf(data.id);
+    if (checked || indeterminate) {
+        // 如果节点被选中且此前还没有添加，就添加到chooseIdList
+        if(index < 0){
+            chooseIdList.value.push(data.id);
+        }
+    } else {
+        // 如果节点被取消选中且已经存在于列表中，则从chooseIdList中移除
+        if (index !== -1) {
+            chooseIdList.value.splice(index, 1);
+        }
+    }
+}
+// 存原来的菜单id列表
+let oldMenuIdList = ref([])
+// 拿到原本就有的菜单列表
+const getOldMenuList = async()=>{
+    // let {data} = await getMenuListByRoleId(role.value.id)
+    let data = [{id: 3, name: '无'}]
+    oldMenuIdList.value = computed(() => {
+        return data.map(item => item.id);
+    });
+    console.log('=========================>>>')
+    console.log(data)
+    console.log(oldMenuIdList.value)
+    console.log('=========================>>>')
+}
+// 获取到所有的树型菜单列表
+const getTreeMenusList = async()=>{
+    let {data} = await getAllTreeMenus()
+    menuTreeList.value = data
+    console.log(data)
+}
+// 存所有的menu
+let menuTreeList = ref([])
 const route = useRoute()
+// 存待操作的role
 let role = ref({})
 onMounted(() => {
     // 拿到对应角色的参数
     role.value.id = route.query.id
     role.value.roleName = route.query.roleName
+    // 初始化菜单数据
+    getTreeMenusList()
+    // 拿原来的数据
+    getOldMenuList()
 })
 </script>
 
@@ -29,8 +87,12 @@ onMounted(() => {
     display: flex;
     justify-content: center;
     flex-wrap: wrap;
-    background: rgb(244,247,252);
+    background: $page-back-color;
     padding: $page-padding;
-
+    .funMode{
+        background: #FFF;
+        width: 100%;
+        padding: 3vh 2vw;
+    }
 }
 </style>
