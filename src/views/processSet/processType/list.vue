98e3509f71688dc449ed15606e265e69e13c3d1f
JamesGosing <3248175240@qq.com>
2024-05-12 23:04:38 +0800

    feat: 初始化了审批类型和模板设置的页面

---
+++ src/views/processSet/processType/list.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="processTypeAll">
+        <div class="funTitle">审批类型列表</div>
+        <div class="baseCard">66</div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+@import url('/src/assets/font-icon/iconfont.css');
+@import '/src/styles/globalPage.scss';
+@import '/src/styles/listSize.scss';
+@import '/src/styles/commonStyles.scss';
+
+.processTypeAll{
+    margin-left: $left-distance;
+    margin-top: $top-distance;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    background: rgb(244,247,252);
+    padding: $page-padding;
+}
+</style>
