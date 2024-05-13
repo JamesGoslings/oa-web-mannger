de89c158afa623f8359dd75b86757d4495713fd7
JamesGosing <3248175240@qq.com>
2024-05-13 20:32:52 +0800

    feat: 完成流程图设计基本展示功能

--- src/views/processSet/processTemplate/list.vue
+++ src/views/processSet/processTemplate/list.vue
@@ -1,34 +1,34 @@
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <template>
     <div class="processTemplateAll">
         <div class="funTitle">审批模板列表</div>
-        <div class="baseCard">66</div>
+        <div class="baseCard bpmnAll">
+            <div id="container"></div>
+        </div>
     </div>
 </template>
 
 <script setup>
+import { markRaw, onMounted } from 'vue';
+import 'bpmn-js/dist/assets/diagram-js.css';
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css';
+import BpmnModeler from 'bpmn-js/lib/Modeler';
+
+let containerEl = ref(null)
+let	bpmnModeler = ref(null)
+
+function init() {
+    containerEl.value = document.getElementById('container');
+    // 加markRaw去除双向绑定作用域
+    bpmnModeler.value = markRaw(new BpmnModeler({
+        container: containerEl.value
+    }));
+    bpmnModeler.value.createDiagram(() => {
+        bpmnModeler.value.get('canvas').zoom('fit-viewport');
+    });
+}
+onMounted(()=>{
+    init()
+})
 
 </script>
 
@@ -46,5 +46,13 @@
     flex-wrap: wrap;
     background: rgb(244,247,252);
     padding: $page-padding;
+    .bpmnAll{
+        width: 100%;
+        height: 80vh;
+        #container{
+            width: 100%;
+            height: 100%;
+        }
+    }
 }
 </style>
