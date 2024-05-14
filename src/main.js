c7003b8d3c6dbdd069471245ab7779d01796f3d8
JamesGosing <3248175240@qq.com>
2024-05-14 19:10:03 +0800

    chore: 挂载了表单设计器

--- src/main.js
+++ src/main.js
@@ -11,11 +11,17 @@ import pinia from '@/utils/pinia.js'
 import vue3TreeOrg from 'vue3-tree-org';
 import "vue3-tree-org/lib/vue3-tree-org.css";
 
+import formCreate from '@form-create/element-ui'
+import FcDesigner from '@form-create/designer'
+
+
 const bus = mitt()
 const app = createApp(App)
 app.use(router)
 app.use(pinia)
 app.use(ElementPlus)
 app.use(vue3TreeOrg)
+app.use(formCreate)
+app.use(FcDesigner)
 app.mount('#app')
 app.config.globalProperties.$bus = bus
